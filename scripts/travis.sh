a2a3b75f1825ef4f81bd9e55a2a9297feff0d31f
Niko Junkala <baabelfish@gmail.com>
2014-05-15 19:49:47 +0300

    Fixed travis script

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,7 @@
 # Fix clang
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
+sudo apt-get -qq install build-essential
 sudo apt-get -qq install llvm
 sudo apt-get -qq install libstdc++-4.8-dev
 
@@ -8,5 +9,5 @@ sudo apt-get -qq install libstdc++-4.8-dev
 sudo pip install cpp-coveralls
 
 cd tests
-clang++ -o ytests *.cpp -std=gnu++11 && ./ytests
+clang++ -o ytests *.cpp -std=gnu++11 -lpthread && ./ytests
 # && coveralls --exclude lib --exclude tests
